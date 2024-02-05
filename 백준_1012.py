5f16104d676f7f0f5d0706c97e057aea1b88bdf8
zzzzzioni <2019121064@yonsei.ac.kr>
2024-02-05 18:45:26 +0900

    5th commit

--- 백준_1012.py
+++ 백준_1012.py
@@ -1,25 +1,36 @@
-# 미해결 
+# from collections import deque
+import sys
 
+# RecursionError 해결
+sys.setrecursionlimit(10**6)
 N = int(input())
 
+def dfs(graph, v, d, l):
+    d[v] = True
+    for i in graph[v]:
+        if not d[l.index(i)]:
+            dfs(graph, l.index(i), d, l)      
+            
 for _ in range(N):
     W = []
-    C = map(int, input().split())
+    C = list(map(int, input().split()))
     for _ in range(C[-1]):
-        W.append(map(int, input().split()))
+        W.append(list(map(int, input().split())))
     
-    i = 0
-    if len(W) == 1:
-        print(i+1)
-    else:
-        pass    
-
+    G = [ [] for _ in range(len(W))]
     for i in range(len(W)):
-        f = []
-        f.append(W[i])
-        for w in f:
-            if 
-        
-        
-        
-    print(len(F))
+        for j in W:
+            if abs(W[i][0] - j[0]) + abs(W[i][1] - j[1]) == 1:
+                G[i].append(j)
+    
+    D = [False for _ in range(len(W))]
+    
+    c = 0
+    for w in range(len(W)):
+        if not D[w]:
+            dfs(G, w, D, W) 
+            c += 1
+        else:
+            pass
+   
+    print(c)
