0d48809bf58dcc4e814d93722d44fd48cca4720a
zzzzzioni <2019121064@yonsei.ac.kr>
2024-07-22 23:43:18 +0900

    feat: 0722 solved (주석 추가)

--- 백준_2217.py
+++ 백준_2217.py
@@ -5,9 +5,10 @@ rope = []
 for i in range(N):
     rope.append(int(input()))
 
+# 정렬하고,
 rope.sort()
-
+# 작은 로프까지 사용했을 때 들 수 있는 최대 하중 계산 
 for i, r in enumerate(rope):
     rope[i] = r * (N-i)
-    
+# 최대 하중 출력 
 print(max(rope))
